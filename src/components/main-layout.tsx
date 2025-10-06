0617527ee305e602c0b96c40c3f6e9bce3d811fc
RedstPhillip <philliplutz01@gmail.com>
2025-10-06 13:09:44 +0200

    feat: add scrollbar styling and overflows

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -5,9 +5,9 @@ import Player from "@/components/player/player.tsx";
 export function MainLayout() {
 	return (
 			<div
-					className="w-full h-screen bg-black text-foreground grid grid-rows-[12fr_1fr] grid-cols-[min-content_1fr] p-2 pb-0 gap-x-2">
+					className="w-full h-screen bg-black text-foreground grid grid-rows-[12fr_1fr] grid-cols-[min-content_1fr] p-2 pb-0 gap-x-2 ">
 				<Sidebar/>
-				<Outlet/>
+				<div className="overflow-y-auto scrollbar scrollbar-track-transparent scrollbar-thumb-neutral-700"> <Outlet/> </div>
 				<Player/>
 			</div>
 	)
